02293b58c7a711f2fd109840b022b760b418bb15
spiralClouds <100466805+spiralClouds@users.noreply.github.com>
2024-03-25 23:29:01 -0400

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use mini_redis::{client, Result};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    /*
+     * TCP connection established between running app and remote address.
+     * Remote address is IP address and port number,
+     * Transmission Control Protocol establishes reliable bidirectional communication
+     *  between two networked devices.
+     * 
+     * Code looks synchronous but is not, when we see await it is not sync
+     * When we make a sync operation the program execution is pauesed until completed
+     * 
+     * Calling an async fn returns a value representing the operation
+     * cenceptually analagous to a zero-argument closure
+     * 
+     * Rust converts an async fn into a routine that operates async at COMPILE TIME
+     * 
+     */
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    client.set("hello", "world".into()).await?;
+    // get hello
+    let result = client.get("hello").await?;
+    println!("retrieved value from server result={:?}", result);
+    println!("Hello, world!");
+    Ok(())
+}
